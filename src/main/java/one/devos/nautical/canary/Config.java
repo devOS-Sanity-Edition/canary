b0c6d30c7d2a41eb6902334df1c85f06af4a84df
TropheusJ <jverrellijr@icloud.com>
2024-02-21 15:06:11 -0500

    improve blockstate addition detection

--- src/main/java/one/devos/nautical/canary/Config.java
+++ src/main/java/one/devos/nautical/canary/Config.java
@@ -2,15 +2,22 @@ package one.devos.nautical.canary;
 
 import java.util.List;
 
+import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-public record Config(List<String> trackedDataWhitelist) {
+public record Config(List<String> trackedDataWhitelist, List<String> stateBuilderWhitelist) {
 	public static final Codec<Config> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			Codec.STRING.listOf().fieldOf("tracked_data_whitelist").forGetter(Config::trackedDataWhitelist)
+			Codec.STRING.listOf()
+					.optionalFieldOf("tracked_data_whitelist", List.of("com.example.mymod.Utilities"))
+					.forGetter(Config::trackedDataWhitelist),
+			Codec.STRING.listOf()
+					.optionalFieldOf("state_builder_whitelist", List.of("net.example.examplemod.Utils"))
+					.forGetter(Config::stateBuilderWhitelist)
 	).apply(instance, Config::new));
 
 	public static Config makeDefault() {
-		return new Config(List.of("com.example.mymod.Utilities"));
+		return CODEC.parse(JsonOps.INSTANCE, new JsonObject()).result().orElseThrow();
 	}
 }
--- src/main/java/one/devos/nautical/canary/mixin/StateDefinition$BuilderMixin.java
+++ src/main/java/one/devos/nautical/canary/mixin/StateDefinition$BuilderMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 
 import net.minecraft.world.level.block.state.properties.Property;
 
+import one.devos.nautical.canary.Canary;
 import one.devos.nautical.canary.CanaryException;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,29 +15,36 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.transformer.meta.MixinMerged;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 @Mixin(StateDefinition.Builder.class)
 public class StateDefinition$BuilderMixin {
 	@Inject(method = "add", at = @At("HEAD"))
 	private void onAdd(Property<?>[] properties, CallbackInfoReturnable<?> cir) {
-		// walk the stack trace to find any calls from mixins
+		// check the stacktrace for calls from mixins
 		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+		// stackTrace[0] = getStackTrace
+		// stackTrace[1] = onAdd
+		// stackTrace[2] = add
+		// stackTrace[3] = caller
+		StackTraceElement caller = stackTrace[3];
+		String callerClassName = caller.getClassName();
+		if (Canary.CONFIG.stateBuilderWhitelist().contains(callerClassName))
+			return; // explicitly declared as safe; ex. util methods
+
 		try {
-			for (StackTraceElement element : stackTrace) {
-				Class<?> clazz = Class.forName(element.getClassName());
-				if (!Block.class.isAssignableFrom(clazz))
-					continue; // not a block, don't care
+			Class<?> clazz = Class.forName(callerClassName);
+			if (!Block.class.isAssignableFrom(clazz))
+				return; // not a block, safe
 
-				for (Method method : getMethodsSafe(clazz)) {
-					if (!method.getName().equals(element.getMethodName()))
-						continue; // only care about the method in the stack trace
+			// find the Method of the caller to see if it's a mixin injection
+			for (Method method : getMethodsSafe(clazz)) {
+				if (!method.getName().equals(caller.getMethodName()))
+					continue; // not it
 
-					MixinMerged mixinMergedMeta = method.getDeclaredAnnotation(MixinMerged.class);
-					if (mixinMergedMeta != null) { // 🚨🚨🚨
-						throw new CanaryException("Unsafe BlockState property addition via mixin [" + mixinMergedMeta.mixin() + "]");
-					}
+				MixinMerged mixinMergedMeta = method.getDeclaredAnnotation(MixinMerged.class);
+				if (mixinMergedMeta != null) { // 🚨🚨🚨
+					throw new CanaryException("Unsafe BlockState property addition via mixin [" + mixinMergedMeta.mixin() + "]");
 				}
 			}
 		} catch (ClassNotFoundException e) {
